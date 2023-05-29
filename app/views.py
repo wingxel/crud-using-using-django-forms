8fcf2d230a100a04f3a404c61988af73b08aa2b2
wingxel <wingxel@gmail.com>
2023-05-29 15:39:57 +0300

    Added index page and configured urls

--- app/views.py
+++ app/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
+from django.core.handlers.wsgi import WSGIRequest
 
-# Create your views here.
+
+class IndexView(View):
+    def get(self, request: WSGIRequest) -> HttpResponse:
+        return render(request, "app/index.html")
